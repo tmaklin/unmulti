2d5d86b1f67074f192ef05507576542483f728b1
Tommi Mäklin <tommi@maklin.fi>
2022-04-01 16:15:07 +0300

    Minimal working program.

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 # unmulti - split a multifasta file into many fasta files
+Note: work-in-progress!
+
+## Installation
+### Compiling from source
+#### Requirements
+- c++11 compliant compiler.
+- cmake v2.8.2 or newer.
+- git.
+- Internet access.
+
+### How-to
+Clone the repository, enter the directory and run
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+
+this will download the necessary dependencies and compile the unmulti executable in build/bin/.
+
+## Usage
+```
+unmulti -f <input multifasta> -o <output directory (default: working directory)
+```
+
+### Example
+Running unmulti on an input file `in.fasta` with the following contents
+
+```
+>seq_1
+AAACGT
+>seq_2
+GGGTAC
+```
+
+will produce files `0.fasta` and `1.fasta` in the output directory with contents
+
+```
+>seq_1
+AAACGT
+```
+
+and
+
+```
+>seq_2
+GGGTAC
+```
+
+### Accepted flags/parameters
+unmulti accepts the following flags/parameters:
+
+```
+-f    Input multifasta.
+-o    Output directory (default: working directory)
+```
 
 ## License
 The source code from this project is subject to the terms of the MIT
--- src/unmulti.cpp
+++ src/unmulti.cpp
@@ -24,12 +24,81 @@
 
 #include "unmulti.hpp"
 
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <exception>
+#include <cstdint>
+
 #include "cxxargs.hpp"
 #include "cxxio.hpp"
 
 #include "version.h"
 
-int main() {
-    cxxargs::Arguments args("unmulti-" + std::string(UNMULTI_BUILD_VERSION), "Usage: unmulti");
+namespace unmulti {
+bool CmdOptionPresent(char **begin, char **end, const std::string &option) {
+  return (std::find(begin, end, option) != end);
+}
+
+void parse_args(int argc, char* argv[], cxxargs::Arguments &args) {
+    args.add_short_argument<std::string>('f', "Input multifasta.");
+    args.add_short_argument<std::string>('o', "Output directory (default: working directory)", ".");
+
+    if (!CmdOptionPresent(argv, argv+argc, "--help")) {
+	args.parse(argc, argv);
+    }
+}
+}
+
+int main(int argc, char* argv[]) {
+    cxxargs::Arguments args("unmulti-" + std::string(UNMULTI_BUILD_VERSION), "Usage: unmulti -f <input multifasta>");
+
+    // Parse input arguments.
+    try {
+	unmulti::parse_args(argc, argv, args);
+
+	if (unmulti::CmdOptionPresent(argv, argv+argc, "--help")) {
+	    std::cerr << "\n" + args.help() << '\n' << '\n';
+	    return 0;
+	}
+    } catch (std::exception &e) {
+	std::cerr << "Parsing arguments failed:\n"
+		  << std::string("\t") + std::string(e.what()) + "\n"
+		  << "\trun unmulti with the --help option for usage instructions.\n";
+	return 1;
+    }
+
+    // Check input and output files.
+    cxxio::In in;
+    try {
+	// Input multifasta exists
+	in.open(args.value<std::string>('f'));
+
+	// Output directory exists
+	cxxio::directory_exists(args.value<std::string>('o'));
+    } catch (std::exception &e) {
+	std::cerr << "I/O error:\n"
+		  << std::string("\t") + std::string(e.what()) + "\n"
+		  << "\trun unmulti with the --help option for usage instructions.\n";
+	return 1;
+    }
+
+    // Process the input
+    std::string line;
+    uint32_t seq_number = 0;
+    cxxio::Out out;
+    while(std::getline(in.stream(), line)) {
+	if (line.at(0) == '>') {
+	    if (seq_number > 0) {
+		out.close(); // Flush only if something has been written.
+	    }
+	    out.open(args.value<std::string>('o') + '/' + std::to_string(seq_number) + ".fasta");
+	    ++seq_number;
+	}
+	out.stream() << line << '\n';
+    }
+    in.close();
+    out.close();
+
     return 0;
 }
